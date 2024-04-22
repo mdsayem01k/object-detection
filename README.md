4649400a848c8cd64a2b74022e7563d3734a2a6a
mdsayem01k <mdsayem01k@gmail.com>
2024-04-22 10:56:23 +0600

    requirements added

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # object-detection
+
+## How to run :
+
+```bash
+conda create -n singLang python=3.7 -y
+```
+
+
+```bash
+conda activate signLang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'md abu sayem',
+    author_email= 'mdsayem01k@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
